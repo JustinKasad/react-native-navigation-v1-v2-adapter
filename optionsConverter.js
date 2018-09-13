c1ba6663cbde86d63f668a4468a70406f8e24a60
JustinKasad <justinkasad@gmail.com>
2018-09-13 11:23:06 -0700

    set default tabBar Button color

--- optionsConverter.js
+++ optionsConverter.js
@@ -28,6 +28,7 @@ export function convertStyle(style = {}, buttons) {
     rootBackgroundImage: style.rootBackgroundImageName,
     modalPresentationStyle: style.modalPresentationStyle,
     topBar: {
+      buttonColor: style.topBarButtonColor,
       visible: style.navBarHidden ? !style.navBarHidden : undefined,
       hideOnScroll: style.navBarHideOnScroll,
       buttonColor: style.navBarButtonColor,
